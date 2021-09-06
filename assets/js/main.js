c5f9e928068f5e6dbf6843cb6b04167d45bb82b0
Carlos Carrillo <carlos.carrillo.carrillo@hotmail.com>
2021-09-06 06:47:08 -0500

    Mostrando y ocultando el menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-console.log('main')
+/* Show and hide menu */
+const   navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
+
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}

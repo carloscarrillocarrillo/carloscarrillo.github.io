883d19a8b74ff8326e428f6ee3b2ba0edb9733d1
Carlos Carrillo <carlos.carrillo@controlt.io>
2026-05-25 06:04:36 -0500

    elimina secciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,34 +139,3 @@ const scrollspy = new IntersectionObserver((entries) => {
 
 sections.forEach(s => scrollspy.observe(s));
 
-/* swiper */
-
-let swiperPortfolio = new Swiper('.portfolio__container', {
-    cssMode: true,
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-}) 
-
-let swiperTestimonial = new Swiper('.testimonial__container', {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 48,
-
-    pagination: {
-        el: '.swiper-pagination-testimonial',
-        clickable: true,
-        dynamicBullets: true,
-    },
-    breakpoints:{
-        568: {
-            slidesPerView: 2,
-        }
-    }
-})
